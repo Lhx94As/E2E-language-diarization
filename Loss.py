0b1cadc3ef5dce6f082b69c9672f159c0bce5d3d
HexinHexin <32285422+Lhx94As@users.noreply.github.com>
2021-03-13 23:19:40 +0800

    Update Loss.py

--- Loss.py
+++ Loss.py
@@ -26,33 +26,3 @@ class DeepClusteringLoss(nn.Module):
         DC_loss = vt_v - 2 * vt_y + yt_y
         return DC_loss/(num_frames**2)
 
-
-
-
-
-class PermutationInvariantLoss(nn.Module):
-    def __init__(self,device):
-        super(PermutationInvariantLoss, self).__init__()
-        self.device = device
-
-    def forward(self, predicts, targets, seq_len):
-        min_loss = 0
-        loss_func = nn.CrossEntropyLoss().to(self.device)
-        predicts = torch.split(predicts, seq_len)
-        targets = torch.split(targets, seq_len)
-        for i in range(len(seq_len)):
-            predicts_ = predicts[i]
-            targets_ = targets[i]
-            n_frames = seq_len[i]
-            label_perms = [targets_[..., list(p)]
-                           for p in permutations(range(targets_.size()[-1]))]
-            min_utt_loss = 0
-            for ii in range(len(label_perms)):
-                label = label_perms[ii]
-                label = torch.argmax(label, dim=-1)
-                loss_ = loss_func(predicts_, label)/n_frames
-                print(loss_)
-                if min_utt_loss > loss_:
-                    min_utt_loss = loss_
-            min_loss += min_utt_loss
-        return min_loss/self.batch_size
