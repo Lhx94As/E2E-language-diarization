78a48a10cdd4d0463e57eaed66939c3acbd4a272
HexinHexin <32285422+Lhx94As@users.noreply.github.com>
2021-04-06 12:35:18 +0800

    Update train_xsa_e2e.py

--- train_xsa_e2e.py
+++ train_xsa_e2e.py
@@ -18,7 +18,7 @@ def setup_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     np.random.seed(seed)
-    torch.backends.cudnn.deterministic = False
+    torch.backends.cudnn.deterministic = False # Do not use True here, otherwise the code will be 10 times slower
     torch.backends.cudnn.benchmark = True
 
 
