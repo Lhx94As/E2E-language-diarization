9c7db845a1943515173f5ada2c53aa3964bf3703
HexinHexin <32285422+Lhx94As@users.noreply.github.com>
2021-04-08 14:28:12 +0800

    Update train_blstm_e2e.py

--- train_blstm_e2e.py
+++ train_blstm_e2e.py
@@ -18,7 +18,7 @@ def setup_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     np.random.seed(seed)
-    torch.backends.cudnn.deterministic = False
+    torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
     
